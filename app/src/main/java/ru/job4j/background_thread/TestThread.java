9fe2eed354161a0225df2f4f80f832f0a7c4d481
yor_email <daniil.stebnitsky@gmail.com>
2020-12-04 11:10:50 +0300

    TestThread.

--- app/src/main/java/ru/job4j/background_thread/TestThread.java
+++ app/src/main/java/ru/job4j/background_thread/TestThread.java
@@ -1,22 +1,15 @@
 package ru.job4j.background_thread;
 
-import android.os.Handler;
 import android.util.Log;
 
 public class TestThread extends Thread {
-    private int time;
-    private int count = 0;
-    TestThread(int time) {
-        this.time = time;
-    }
     @Override
     public void run() {
-        Handler handler = MainActivity.getHandler();
-        while (count != time && !isInterrupted()) {
+        int count = 0;
+        while (!isInterrupted()) {
             String TAG = "Runnable_tag";
             Log.d(TAG, "Thread count: " + count);
             ++count;
-            handler.post(() -> MainActivity.setThreadCountText(count));
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
